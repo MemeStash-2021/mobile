d49a76518339632a4edcb82ac2315072e3cbaa55
Bo <bo.robbrecht@student.howest.be>
2021-03-02 20:34:13 +0100

    Init commit

---
+++ app/src/main/java/be/howest/borobbrecht/memestash/MainActivity.kt
@@ -0,0 +1,11 @@
+package be.howest.borobbrecht.memestash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
