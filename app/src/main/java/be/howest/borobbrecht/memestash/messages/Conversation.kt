80495a0c926b25dfc02bc79a446d946ef694074c
Bo <bo.robbrecht@student.howest.be>
2021-03-25 14:32:34 +0100

    added basic files for Conversation view

---
+++ app/src/main/java/be/howest/borobbrecht/memestash/messages/Conversation.kt
@@ -0,0 +1,12 @@
+package be.howest.borobbrecht.memestash.messages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import be.howest.borobbrecht.memestash.R
+
+class Conversation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conversation)
+    }
+}
