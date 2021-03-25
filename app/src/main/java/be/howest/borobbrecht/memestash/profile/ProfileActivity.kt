a2d23e3a23f93a782987ea99f738fb7dd69cce97
Bo <bo.robbrecht@student.howest.be>
2021-03-25 11:15:55 +0100

    Added picture & recyclerview

---
+++ app/src/main/java/be/howest/borobbrecht/memestash/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package be.howest.borobbrecht.memestash.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import be.howest.borobbrecht.memestash.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
